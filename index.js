c65083e294a571b4c96511153ff200eb951cb719
wl4dig3 <wladige@gmail.com>
2024-01-20 23:33:10 -0300

    desafio 3 condiciones

---
+++ index.js
@@ -0,0 +1,16 @@
+function ingresar() {
+  let select1 = document.querySelector("#select-1").value;
+  let select2 = document.querySelector("#select-2").value;
+  let select3 = document.querySelector("#select-3").value;
+console.log('::>', select1, select2, select3);
+  if (select1 === "9" && select2 === "1" && select3 === "1") {
+    resultado("el password 1 es correcto");
+  } else if (select1 == "7" && select2 == "1" && select3 == "4") {
+    resultado("El password 2 es correcto");
+  } else {
+    resultado("El password ingresado no es correcto");
+  }
+};
+function resultado(mensaje) {
+  document.querySelector("#resultado").innerHTML = mensaje;
+};
